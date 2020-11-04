f697651fab9d6545da7755e0347b756f416b45ea
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-11-05 00:59:42 +0800

    my attempt at updating the database

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const session = require('express-session');
-const passport=require('passport')
-const setupPassport=require("./passport");
+const session = require("express-session");
+const passport = require("passport");
+const setupPassport = require("./passport");
 const knexfile = require("./knexfile");
 const app = express();
-const db=require('./db');
+const db = require("./db");
 
 // server session setup
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 const port = 5000;
 
 //middleware setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + "/public"));
 
 setupPassport(app);
-
 
 app.set("view engine", "ejs");
 
@@ -37,39 +38,70 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-
 //function to check if the user is authenicated
-function isLoggedIn(req,res,next){
-  if(req.isAuthenticated()){
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 //GET the the loggedin dashboard page
-app.get("/dashboard",isLoggedIn,(req,res)=>{
-// res.render('dashboard')
-res.render("createProfile");
-})
+app.get("/dashboard", isLoggedIn, (req, res) => {
+  // res.render('dashboard')
+  res.render("createProfile");
+});
+
+app.post("/create", isLoggedIn, async (req, res) => {
+  try {
+    // INSERT INTO users()
+    let userProfile = {
+      nickname: req.body.nickname,
+      photo: req.body.photo,
+      gender: req.body.gender,
+      description: req.body.description,
+    };
+    db.update(userProfile)
+      .where("id", req.params.id)
+      .returning("*")
+      .into("users")
+      .then(function (data) {
+        res.send(data);
+      });
+    console.log(req.body);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //GET the login page
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
   // res.render('dashboard')
-  res.render("loginRegister")
-  })
+  res.render("loginRegister");
+});
 
 //POST login
-app.post('/login', passport.authenticate('local-login', {
-  successRedirect: '/dashboard',
-  failureRedirect: '/error'
-}));
+app.post(
+  "/login",
+  (req, res, next) => {
+    console.log(`hi`);
+    console.log(req.body.username);
+    next();
+  },
+  passport.authenticate("local-login", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/error",
+  })
+);
 
 //POST register
-app.post('/signup', passport.authenticate('local-signup', {
-  successRedirect: '/login',
-  failureRedirect: '/error'
-}));
-
+app.post(
+  "/signup",
+  passport.authenticate("local-signup", {
+    successRedirect: "/login",
+    failureRedirect: "/error",
+  })
+);
 
 //set up the server
 app.listen(port, () => {
